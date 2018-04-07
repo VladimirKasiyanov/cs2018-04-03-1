4d4be757cff8fc361bb4ded5592fe33aad2e7e96
VladimirKasiyanov <vladimirqw@gmail.com>
2018-04-07 09:02:49 +0300

    lesson2 BC

--- src/by/it/KasiyanovVladimir/lesson02/TaskC1.java
+++ src/by/it/KasiyanovVladimir/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.KasiyanovVladimir.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,9 +12,13 @@ package by.it.KasiyanovVladimir.lesson02;
 Sum = 26
 */
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
+
+        System.out.println("Sum = " + (number1 + number2));
 
 
-
-
-
+    }
 }
--- src/by/it/KasiyanovVladimir/lesson02/TaskC2.java
+++ src/by/it/KasiyanovVladimir/lesson02/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.KasiyanovVladimir.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -14,6 +16,29 @@ HEX:22+1a=3c
 OCT:42+32=74
 */
 class TaskC2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
 
+        System.out.println("DEC:" + number1 + "+" + number2 + "=" + (number1 + number2));
+
+        String BIN1 = Integer.toBinaryString(number1);
+        String BIN2 = Integer.toBinaryString(number2);
+        String BIN = Integer.toBinaryString(number1 + number2);
+        System.out.println("BIN:" + BIN1 + "+" + BIN2 + "=" + BIN);
+
+        String HEX1 = Integer.toHexString(number1);
+        String HEX2 = Integer.toHexString(number2);
+        String HEX = Integer.toHexString(number1 + number2);
+        System.out.println("HEX:" + HEX1 + "+" + HEX2 + "=" + HEX);
+
+        String Octal1 = Integer.toOctalString(number1);
+        String Octal2 = Integer.toOctalString(number2);
+        String Octal = Integer.toOctalString(number1 + number2);
+        System.out.println("OCT:" + Octal1 + "+" + Octal2 + "=" + Octal);
+
+
+    }
 
 }
